7dceba4219b72a86125f313f5477e53244dc4ebc
Bill Oxbury <oxburybill@gmail.com>
2023-09-02 12:21:53 +0100

    Add coinflip

--- coinflip/binomial.go
+++ coinflip/binomial.go
@@ -8,9 +8,13 @@ func binomial(N int, p float64) int {
 
 	h := 0
 	for i := 0; i < N; i++ {
-		if rand.Float64() < p {
+		if heads(p) {
 			h++
 		}
 	}
 	return h
+}
+
+func heads(p float64) bool {
+	return rand.Float64() < p
 }
