5b033c4f5daad5c2014c7e8fee128586b591e17c
Bill Oxbury <oxburybill@gmail.com>
2023-09-05 10:36:27 +0100

    Profiling code and output

--- gaussian/go.mod
+++ gaussian/go.mod
@@ -1,3 +1,10 @@
 module github.com/billoxbury/goformath/gaussian
 
 go 1.20
+
+require (
+	github.com/chzyer/readline v1.5.1 // indirect
+	github.com/google/pprof v0.0.0-20230901174712-0191c66da455 // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20230524184225-eabc099b10ab // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
--- gaussian/main.go
+++ gaussian/main.go
@@ -19,7 +19,18 @@ Run with:
 
 > go build .
 > ./gaussian -h
-> ./gaussian -w 2
+> ./gaussian -w 2 > output.txt
+
+For profile:
+
+go tool pprof -pdf ./gaussian ./prof/randS.prof > ./prof/randS.pdf
+go tool pprof -pdf ./gaussian ./prof/randP.prof > ./prof/randP.pdf
+go tool pprof -pdf ./gaussian ./prof/polarS.prof > ./prof/polarS.pdf
+go tool pprof -pdf ./gaussian ./prof/polarP.prof > ./prof/polarP.pdf
+go tool pprof -pdf ./gaussian ./prof/cartS.prof > ./prof/cartS.pdf
+go tool pprof -pdf ./gaussian ./prof/cartP.prof > ./prof/cartP.pdf
+
+etc
 
 */
 
@@ -29,10 +40,16 @@ import (
 	"flag"
 	"fmt"
 	"math/rand"
+	"os"
+	"runtime/pprof"
 	"time"
 )
 
 func main() {
+
+	f, _ := os.Create("gaussian.prof")
+	pprof.StartCPUProfile(f)
+	pprof.StopCPUProfile()
 
 	// cmd line arguments
 	var n, w int
@@ -43,52 +60,81 @@ func main() {
 	// run rnorm generators
 	// 1.
 	fmt.Println("Serial sample (rand):")
+
+	f1, _ := os.Create("randS.prof")
+	pprof.StartCPUProfile(f1)
 	start1 := time.Now()
 	x1 := rnorm(n, rand.NormFloat64)
 	tStd := time.Since(start1)
+	pprof.StopCPUProfile()
+
 	fmt.Printf("Time:\t\t%v\n", tStd)
 	summary(x1)
 
 	// 2.
 	fmt.Println("Parallel sample (rand):")
+
+	f2, _ := os.Create("randP.prof")
+	pprof.StartCPUProfile(f2)
 	start2 := time.Now()
 	x2 := rnormPar(n, w, rand.NormFloat64)
 	tStdPar := time.Since(start2)
+	pprof.StopCPUProfile()
+
 	fmt.Printf("Time:\t\t%v\n", tStdPar)
 	summary(x2)
 
 	// 3.
 	fmt.Println("Serial Box-Muller polar:")
+
+	f3, _ := os.Create("polarS.prof")
+	pprof.StartCPUProfile(f3)
 	start3 := time.Now()
 	x3 := rnorm(n, rnormBoxMullerPolar)
 	tBMpol := time.Since(start3)
+	pprof.StopCPUProfile()
+
 	fmt.Printf("Time:\t\t%v\n", tBMpol)
 	summary(x3)
 
 	// 4.
 	fmt.Println("Parallel Box-Muller polar:")
+
+	f4, _ := os.Create("polarP.prof")
+	pprof.StartCPUProfile(f4)
 	start4 := time.Now()
 	x4 := rnormPar(n, w, rnormBoxMullerPolar)
 	tBMpolPar := time.Since(start4)
+	pprof.StopCPUProfile()
+
 	fmt.Printf("Time:\t\t%v\n", tBMpolPar)
 	summary(x4)
 
 	// 5.
 	fmt.Println("Serial Box-Muller cartesian:")
+
+	f5, _ := os.Create("cartS.prof")
+	pprof.StartCPUProfile(f5)
 	start5 := time.Now()
 	x5 := rnorm(n, rnormBoxMullerCart)
 	tBMcart := time.Since(start5)
+	pprof.StopCPUProfile()
+
 	fmt.Printf("Time:\t\t%v\n", tBMcart)
 	summary(x5)
 
 	// 6.
 	fmt.Println("Parallel Box-Muller cartesian:")
+
+	f6, _ := os.Create("cartP.prof")
+	pprof.StartCPUProfile(f6)
 	start6 := time.Now()
 	x6 := rnormPar(n, w, rnormBoxMullerCart)
 	tBMcartPar := time.Since(start6)
+	pprof.StopCPUProfile()
+
 	fmt.Printf("Time:\t\t%v\n", tBMcartPar)
 	summary(x6)
-
 }
 
 // summary stats
